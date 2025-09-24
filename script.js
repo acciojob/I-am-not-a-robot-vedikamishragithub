60f7301cdd63ca80d96b5f4535488ceeca615d1b
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2025-09-24 23:44:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+function shuffleArray(array) {
+	for(i=0;i<array.length;i++){
+      let j=Math.floor(Math.random()*i+1);
+	  [array[i],array[j]]=[array[j],array[i]];	
+	}
+	return array;
+}
+ function randomImage() {
+ 	const container=document.getElementById('container');
+	const images=Array.from(container.querySelectorAll("img"));
+   
+	 const shuffle=shuffleArray(images);
+
+	 shuffle.forEach(img=>{
+       container.appendChild(img);
+	 })
+	 
+ }
+
+window.addEventListener('load', shuffleImages);
